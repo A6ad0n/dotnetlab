a0cec39446b7c2d7e533a81b7eb088bcf7eac7c5
Egor Bazakin <egor.bazakin.09@gmail.com>
2025-12-13 17:49:25 +0300

    DAL: Migration — added categories and statuses

--- src/PizzaApp.DataAccess/Context/DbInitializer.cs
+++ src/PizzaApp.DataAccess/Context/DbInitializer.cs
@@ -15,6 +15,28 @@ public static class SeedConstants
         public static readonly Guid RoleCustomer = Guid.Parse("00000000-0000-0000-0000-000000000003");
         public static readonly Guid RoleDeveloper = Guid.Parse("00000000-0000-0000-0000-000000000004");
         
+        public static readonly Guid C1 = Guid.Parse("00000000-0000-0000-0000-000000000011");
+        public static readonly Guid C2 = Guid.Parse("00000000-0000-0000-0000-000000000022");
+        public static readonly Guid C3 = Guid.Parse("00000000-0000-0000-0000-000000000033");
+        public static readonly Guid C4 = Guid.Parse("00000000-0000-0000-0000-000000000044");
+        public static readonly Guid C5 = Guid.Parse("00000000-0000-0000-0000-000000000055");
+        public static readonly Guid C6 = Guid.Parse("00000000-0000-0000-0000-000000000066");
+        public static readonly Guid C7 = Guid.Parse("00000000-0000-0000-0000-000000000077");
+        
+        public static readonly Guid S1 = Guid.Parse("00000000-0000-0000-0000-000000000111");
+        public static readonly Guid S2 = Guid.Parse("00000000-0000-0000-0000-000000000222");
+        public static readonly Guid S3 = Guid.Parse("00000000-0000-0000-0000-000000000333");
+        public static readonly Guid S4 = Guid.Parse("00000000-0000-0000-0000-000000000444");
+        public static readonly Guid S5 = Guid.Parse("00000000-0000-0000-0000-000000000555");
+        public static readonly Guid S6 = Guid.Parse("00000000-0000-0000-0000-000000000666");
+        public static readonly Guid S7 = Guid.Parse("00000000-0000-0000-0000-000000000777");
+        public static readonly Guid S8 = Guid.Parse("00000000-0000-0000-0000-000000000888");
+        public static readonly Guid S9 = Guid.Parse("00000000-0000-0000-0000-000000000999");
+        public static readonly Guid S10 = Guid.Parse("00000000-0000-0000-0000-000000001111");
+        public static readonly Guid S11 = Guid.Parse("00000000-0000-0000-0000-000000002222");
+        public static readonly Guid S12 = Guid.Parse("00000000-0000-0000-0000-000000003333");
+        public static readonly Guid S13 = Guid.Parse("00000000-0000-0000-0000-000000004444");
+        
         public static readonly Guid UserAdmin = Guid.Parse("00000000-0000-0000-0000-000000000101");
         
         public static readonly Guid UserRoleAdmin = Guid.Parse("00000000-0000-0000-0000-000000000201");
@@ -34,7 +56,8 @@ public static class DbInitializer
         SeedUsers(modelBuilder);
         SeedUserRoles(modelBuilder);
     }
-    public static void SeedRoles(ModelBuilder modelBuilder)
+
+    private static void SeedRoles(ModelBuilder modelBuilder)
     {
         var roles = new List<RoleEntity>
         {
@@ -79,7 +102,7 @@ public static class DbInitializer
         modelBuilder.Entity<RoleEntity>().HasData(roles);
     }
 
-    public static void SeedUsers(ModelBuilder modelBuilder)
+    private static void SeedUsers(ModelBuilder modelBuilder)
     {
         var users = new List<UserEntity>
         {
@@ -103,7 +126,7 @@ public static class DbInitializer
         modelBuilder.Entity<UserEntity>().HasData(users);
     }
 
-    public static void SeedUserRoles(ModelBuilder modelBuilder)
+    private static void SeedUserRoles(ModelBuilder modelBuilder)
     {
         var userRoles = new List<UserRoleEntity>
         {
@@ -118,5 +141,183 @@ public static class DbInitializer
             }
         };
         modelBuilder.Entity<UserRoleEntity>().HasData(userRoles);
+    }
+
+    private static void SeedCategories(ModelBuilder modelBuilder)
+    {
+        var categories = new List<MenuCategoryEntity>
+        {
+            new MenuCategoryEntity
+            {
+                Id = 1,
+                ExternalId = SeedConstants.Guids.C1,
+                Name = MenuCategory.Pizza,
+                CreationTime = SeedConstants.SeedDate,
+                ModificationTime = SeedConstants.SeedDate
+            },
+            new MenuCategoryEntity
+            {
+                Id = 2,
+                ExternalId = SeedConstants.Guids.C2,
+                Name = MenuCategory.Drink,
+                CreationTime = SeedConstants.SeedDate,
+                ModificationTime = SeedConstants.SeedDate
+            },
+            new MenuCategoryEntity
+            {
+                Id = 3,
+                ExternalId = SeedConstants.Guids.C3,
+                Name = MenuCategory.Sauce,
+                CreationTime = SeedConstants.SeedDate,
+                ModificationTime = SeedConstants.SeedDate
+            },
+            new MenuCategoryEntity
+            {
+                Id = 4,
+                ExternalId = SeedConstants.Guids.C4,
+                Name = MenuCategory.Dessert,
+                CreationTime = SeedConstants.SeedDate,
+                ModificationTime = SeedConstants.SeedDate
+            },
+            new MenuCategoryEntity
+            {
+                Id = 5,
+                ExternalId = SeedConstants.Guids.C5,
+                Name = MenuCategory.Snack,
+                CreationTime = SeedConstants.SeedDate,
+                ModificationTime = SeedConstants.SeedDate
+            },
+            new MenuCategoryEntity
+            {
+                Id = 6,
+                ExternalId = SeedConstants.Guids.C6,
+                Name = MenuCategory.Combo,
+                CreationTime = SeedConstants.SeedDate,
+                ModificationTime = SeedConstants.SeedDate
+            },
+            new MenuCategoryEntity
+            {
+                Id = 7,
+                ExternalId = SeedConstants.Guids.C7,
+                Name = MenuCategory.SpecialOffer,
+                CreationTime = SeedConstants.SeedDate,
+                ModificationTime = SeedConstants.SeedDate
+            }
+        };
+        
+        modelBuilder.Entity<MenuCategoryEntity>().HasData(categories);
+    }
+
+    private static void SeedStatuses(ModelBuilder modelBuilder)
+    {
+        var statuses = new List<StatusEntity>
+        {
+            new StatusEntity
+            {
+                Id = 1,
+                ExternalId = SeedConstants.Guids.S1,
+                Name = Status.OrderCreated,
+                CreationTime = SeedConstants.SeedDate,
+                ModificationTime = SeedConstants.SeedDate
+            },
+            new StatusEntity
+            {
+                Id = 2,
+                ExternalId = SeedConstants.Guids.S2,
+                Name = Status.OrderConfirmed,
+                CreationTime = SeedConstants.SeedDate,
+                ModificationTime = SeedConstants.SeedDate
+            },
+            new StatusEntity
+            {
+                Id = 3,
+                ExternalId = SeedConstants.Guids.S3,
+                Name = Status.OrderPreparing,
+                CreationTime = SeedConstants.SeedDate,
+                ModificationTime = SeedConstants.SeedDate
+            },
+            new StatusEntity
+            {
+                Id = 4,
+                ExternalId = SeedConstants.Guids.S4,
+                Name = Status.OrderReady,
+                CreationTime = SeedConstants.SeedDate,
+                ModificationTime = SeedConstants.SeedDate
+            },
+            new StatusEntity
+            {
+                Id = 5,
+                ExternalId = SeedConstants.Guids.S5,
+                Name = Status.OrderDelivering,
+                CreationTime = SeedConstants.SeedDate,
+                ModificationTime = SeedConstants.SeedDate
+            },
+            new StatusEntity
+            {
+                Id = 6,
+                ExternalId = SeedConstants.Guids.S6,
+                Name = Status.OrderCompleted,
+                CreationTime = SeedConstants.SeedDate,
+                ModificationTime = SeedConstants.SeedDate
+            },
+            new StatusEntity
+            {
+                Id = 7,
+                ExternalId = SeedConstants.Guids.S7,
+                Name = Status.OrderCancelled,
+                CreationTime = SeedConstants.SeedDate,
+                ModificationTime = SeedConstants.SeedDate
+            },
+            new StatusEntity
+            {
+                Id = 8,
+                ExternalId = SeedConstants.Guids.S8,
+                Name = Status.MenuActive,
+                CreationTime = SeedConstants.SeedDate,
+                ModificationTime = SeedConstants.SeedDate
+            },
+            new StatusEntity
+            {
+                Id = 9,
+                ExternalId = SeedConstants.Guids.S9,
+                Name = Status.MenuInactive,
+                CreationTime = SeedConstants.SeedDate,
+                ModificationTime = SeedConstants.SeedDate
+            },
+            new StatusEntity
+            {
+                Id = 10,
+                ExternalId = SeedConstants.Guids.S10,
+                Name = Status.MenuArchived,
+                CreationTime = SeedConstants.SeedDate,
+                ModificationTime = SeedConstants.SeedDate
+            },
+            new StatusEntity
+            {
+                Id = 11,
+                ExternalId = SeedConstants.Guids.S11,
+                Name = Status.DiscountActive,
+                CreationTime = SeedConstants.SeedDate,
+                ModificationTime = SeedConstants.SeedDate
+            },
+            new StatusEntity
+            {
+                Id = 12,
+                ExternalId = SeedConstants.Guids.S12,
+                Name = Status.DiscountExpired,
+                CreationTime = SeedConstants.SeedDate,
+                ModificationTime = SeedConstants.SeedDate
+            },
+            new StatusEntity
+            {
+                Id = 13,
+                ExternalId = SeedConstants.Guids.S13,
+                Name = Status.DiscountScheduled,
+                CreationTime = SeedConstants.SeedDate,
+                ModificationTime = SeedConstants.SeedDate
+            }
+        };
+        
+        modelBuilder.Entity<StatusEntity>().HasData(statuses);
     }
 }
